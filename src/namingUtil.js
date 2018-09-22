d0e4ed6190116a6bcb15050fc5ebcaa28ed13c5e
Beta Faccion <betafcc@gmail.com>
2018-09-22 15:59:10 -0300

    Adds namingUtil

--- src/namingUtil.js
+++ src/namingUtil.js
@@ -1,7 +1,29 @@
+const {readdir} = require('fs').promises
 const moment = require('moment')
 const slugify = require('@sindresorhus/slugify')
 
 const {exec} = require('./util')
+
+const regex = {
+  project: /^\d\d\d\d-\d\d-\d\d-[a-z]+-[a-z0-9\-]+[a-z0-9]$/,
+  notebook: /^\d+-\d\d\d\d-\d\d-\d\d-[a-z]+-[a-z0-9\-]+[a-z0-9].ipynb$/
+}
+
+const parse = str =>
+  (regex.project.test(str)) ? {type: 'project', ..._parseProject(str)}
+    : (regex.notebook.test(str)) ? {type: 'notebook', ..._parseNotebook(str.slice(0, -6))}
+      : null
+
+const _parseProject = str => ({
+  date: moment(str.slice(0, 10) + 'T00:00:00Z'),
+  initials: str.slice(11).match(/^\w+/)[0],
+  nameSlug: str.slice(11).match(/^\w+-([a-z0-9\-]+[a-z0-9])$/)[1]
+})
+
+const _parseNotebook = str => {
+  const [_, n, rest] = str.match(/^(\d+)-(.+)$/)
+  return {index: Number.parseInt(n), ..._parseProject(rest)}
+}
 
 const project = async ({
   date,
@@ -26,10 +48,31 @@ const _project = ({
   slugify(name)
 )
 
+const notebook = async ({
+  index,
+  date,
+  author,
+  name
+}) =>
+  (index === undefined) ? await notebook({
+    index: 1 + (await _currentDirMaxIndex() || -1),
+    date,
+    author,
+    name
+  })
+    : index + '-' + await project({index, date, author, name})
+
+const _currentDirMaxIndex = async () =>
+  (await readdir(process.cwd()))
+    .map(parse)
+    .filter(el => !!el)
+    .map(el => el.index)
+    .sort()
+    [0]
+
 module.exports = {
-  // regex,
-  // parse,
+  regex,
+  parse,
   project,
-  _project
-  // notebook
+  notebook
 }
